42537700ba24a8c59b5c045a8501e847dfbd4749
Morayo Kolawole <morayo.dev@gmail.com>
2023-05-04 04:29:23 +0100

    code refactoring and fixing of bugs

--- src/main.js
+++ src/main.js
@@ -3,146 +3,80 @@ const checkBtn = document.getElementById('check-btn');
 const taskContainer = document.getElementById('tasks-container');
 const inputContainer = document.getElementById('input-item');
 
-//DATE
-
-
-
-function getTimeFormat(hour) {
-  return hour >= 12 ? "PM" : "AM";
-}
-
-//Create new elements
-let div = document.createElement('div');
-let li = document.createElement('li');
-
 //get input text
 let inputText = document.getElementById('input-item');
-
-
-//add new tasks
+/*============================== EVENT LISTENERS============================== */
+//Event Listeners for the button click events that add new tasks
 checkBtn.addEventListener('click', () => {
   if (inputText.value.length !== 0) {
-
-    //time
-    const date = new Date();
-
-    // let res = /\w+ \w+ \d+ \d+ \d\d/.exec(date);
-    let res = /\w+ \w+ \d+ \d+ \d\d:\d\d/.exec(date);
-
-    let array = res[0].split(" ");
-    let [hours, minutes] = array[4].split(":");
-    let houred = hours % 12;
-    let month = array[1];
-    let dayNumber = array[2];
-
     //create elements
-    let divOuterContainer = document.createElement('div');
-    let divInnerContainer = document.createElement('div')
-    let icon1 = document.createElement("i");
-    let icon2 = document.createElement("i");
-    let span = document.createElement('span');
+    let toDoOuterContainer = createElement('div');
+    let toDoInnerContainer = createElement('div')
+    let doneIcon = createElement("i");
+    let deleteIcon = createElement("i");
+    let span = createElement('span');
 
-    //set attributes
-    divOuterContainer.className = "tasks"
-    span.className = "time"
-    divInnerContainer.className = "task-content";
-    icon1.className = "fa-solid fa-check-double task-icon done";
-    icon2.className = "fa-solid fa-trash task-icon delete";
+    //the hours is a 24hr clock format hence the conversion below
+    const [month, dayNumber, hours, minutes] = time();
+    const twelveHourFormat = hours % 12;
 
+    //for each element set their corresponding class attribute
+    setMultipleElementAttributes(
+      [toDoOuterContainer, toDoInnerContainer, doneIcon, deleteIcon, span],
+      ["tasks", "task-content", "fa-solid fa-check-double task-icon done", "fa-solid fa-trash task-icon delete", "time"]);
 
+    toDoInnerContainer.innerHTML = `<li class="task-item"> ${inputText.value}</li> <span class="time" id="times">${dayNumber} ${month} ${hours < 12 ? twelveHourFormat : hours}:${minutes} ${getTimeFormat(hours)}</span>`;
 
-    //set the text content of li to the input text
-    // li.innerText = inputText.value;
-    divInnerContainer.innerHTML = `<li class="task-item"> ${inputText.value}</li> <span class="time" id="times">${dayNumber} ${month} ${hours < 12 ? houred : hours}:${minutes} ${getTimeFormat(hours)}</span>`;
-
-
-
-    divOuterContainer.appendChild(divInnerContainer);
-    divOuterContainer.appendChild(icon1);
-    divOuterContainer.appendChild(icon2);
-    taskContainer.prepend(divOuterContainer);
+    multiChildAppend(toDoOuterContainer, [toDoInnerContainer, doneIcon, deleteIcon]);
+    taskContainer.prepend(toDoOuterContainer);
     inputText.value = '';
   }
 });
 
+
+//Event Listener for the ENTER key event that add new tasks
 inputContainer.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
 
     if (inputText.value.length !== 0) {
-      //time
-      const date = new Date();
-
-      // let res = /\w+ \w+ \d+ \d+ \d\d/.exec(date);
-      let res = /\w+ \w+ \d+ \d+ \d\d:\d\d/.exec(date);
-
-      let array = res[0].split(" ");
-      let [hours, minutes] = array[4].split(":");
-      let houred = hours % 12;
-      let month = array[1];
-      let dayNumber = array[2];
-
       //create elements
-      let divOuterContainer = document.createElement('div');
-      let divInnerContainer = document.createElement('div')
-      let icon1 = document.createElement("i");
-      let icon2 = document.createElement("i");
-      let span = document.createElement('span');
+      let toDoOuterContainer = createElement('div');
+      let toDoInnerContainer = createElement('div')
+      let doneIcon = createElement("i");
+      let deleteIcon = createElement("i");
+      let span = createElement('span');
 
-      //set attributes
-      divOuterContainer.className = "tasks"
-      span.className = "time"
-      divInnerContainer.className = "task-content";
-      icon1.className = "fa-solid fa-check-double task-icon done";
-      icon2.className = "fa-solid fa-trash task-icon delete";
+      //the hours is a 24hr clock format hence the conversion below
+      const [month, dayNumber, hours, minutes] = time();
+      const twelveHourFormat = hours % 12;
 
+      //for each element set their corresponding class attribute
+      setMultipleElementAttributes(
+        [toDoOuterContainer, toDoInnerContainer, doneIcon, deleteIcon, span],
+        ["tasks", "task-content", "fa-solid fa-check-double task-icon done", "fa-solid fa-trash task-icon delete", "time"]);
 
-
-      //set the text content of li to the input text
-      // li.innerText = inputText.value;
-      divInnerContainer.innerHTML = `<li class="task-item"> ${inputText.value}</li> <span class="time" id="times">${dayNumber} ${month} ${hours < 12 ? houred : hours}:${minutes} ${getTimeFormat(hours)}</span>`;
+      toDoInnerContainer.innerHTML = `<li class="task-item"> ${inputText.value}</li> <span class="time" id="times">${dayNumber} ${month} ${hours < 12 ? twelveHourFormat : hours}:${minutes} ${getTimeFormat(hours)}</span>`;
 
 
-
-      divOuterContainer.appendChild(divInnerContainer);
-      divOuterContainer.appendChild(icon1);
-      divOuterContainer.appendChild(icon2);
-      taskContainer.prepend(divOuterContainer);
+      multiChildAppend(toDoOuterContainer, [toDoInnerContainer, doneIcon, deleteIcon]);
+      taskContainer.prepend(toDoOuterContainer);
       inputText.value = '';
     }
   }
 });
 
-//move the list with class of line-through down as received
-//remove the line through class from the time text
-
-
-
-//complete tasks
+//complete tasks should have their content underlined if the done button is clicked
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains("done")) {
     const task = e.target.previousElementSibling.firstElementChild;
     task.classList.toggle("line-through");
-
-    let tasksArray = taskContainer.childNodes;
-
-
-    console.log(tasksArray);
-    console.log(task.parentElement);
-    console.log(task.parentElement.parentElement === tasksArray[1]);
-
-    taskContainer.insertBefore(task.parentElement.parentElement, tasksArray[tasksArray.length])
-
-    // task.parentElement
-
-
+    // let tasksArray = taskContainer.childNodes;
+    // taskContainer.insertBefore(task.parentElement.parentElement, tasksArray[tasksArray.length])
   }
-
-  //move the task down
-
 });
 
 
-//Delete task
+//Delete task when the trash icon button is clicked
 document.getElementById('tasks-container').addEventListener('click', (e) => {
   if (e.target.classList.contains("delete")) {
     if (confirm("Are you sure you want to delete?")) {
@@ -150,10 +84,12 @@ document.getElementById('tasks-container').addEventListener('click', (e) => {
     }
   }
 });
+// ===================== END ===================================
 
 
-
-//confirm that the task container is not empty
+/* ============================== TIME EVENTS ================================== */
+//This alllow the tasks container to be shown if it is not empty
+//By checking every 0.1secs
 setInterval(() => {
   if (taskContainer.childNodes.length !== 0) {
     taskContainer.style.display = 'block';
@@ -161,3 +97,44 @@ setInterval(() => {
     taskContainer.style.display = 'none';
   }
 }, 100);
+
+
+//======================  END ===================================
+
+/* ======================== FUNCTIONS DEFINITION ================================= */
+function getTimeFormat(hour) {
+  return hour >= 12 ? "PM" : "AM";
+}
+
+
+//return the month, day, and time of the day a certain event occurs.
+function time() {
+  const date = new Date();
+  let [_, month, dayNumber, __, hourMin] = /\w+ \w+ \d+ \d+ \d\d:\d\d/.exec(date)[0].split(" ");
+  let [hours, minutes] = hourMin.split(":");
+
+  return [month, dayNumber, hours, minutes];
+}
+
+//Although this is self explanatory, this function creates and returns an HTMLElement
+function createElement(element) {
+  return document.createElement(element);
+}
+
+//this set a class attribute for each corresponding element
+function setMultipleElementAttributes([...element], [...attr]) {
+  for (let i = 0; i < element.length; i++) {
+    for (let k = 0; k < attr.length; k++) {
+      if (i === k) {
+        element[i].className = attr[k];
+      }
+    }
+  }
+}
+
+//this allow many childNode/Element to be appended to a parent element
+function multiChildAppend(parent, [...child]) {
+  for (let j = 0; j < child.length; j++) {
+    parent.appendChild(child[j]);
+  }
+}
